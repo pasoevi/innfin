15a84aae0edee68f57dd710792131e5f7f6ecf04
Sergi Pasoev <s.pasoev@gmail.com>
2016-03-27 16:24:20 +0400

    basic log container

--- src/gui.c
+++ src/gui.c
@@ -1,16 +1,33 @@
 #include "gui.h"
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
 
 static void render_bar(struct engine *engine, int x, int y, int w,
                        const char *name, const float value,
                        const float max_value, const TCOD_color_t bar_col,
                        const TCOD_color_t back_col){
-        
         TCOD_console_set_default_background(engine->gui->con, back_col);
         TCOD_console_rect(engine->gui->con, x, y, BAR_W, 1, false, TCOD_BKGND_SET);
         TCOD_console_print_ex(engine->gui->con, x + w/2, y, TCOD_BKGND_NONE, TCOD_CENTER,
                               "%s : %g/%g", name, value, max_value);
+}
+
+static void render_log(struct engine *engine, int startx , int starty){
+        /* draw the message log */
+        struct message **iter;
+        float color_coef = 0.4f;
+        int y = starty;
+        for(iter = (struct message **)TCOD_list_begin(engine->gui->log);
+            iter != (struct message **)TCOD_list_end(engine->gui->log);
+            iter++){
+                TCOD_console_set_default_foreground(engine->gui->con, TCOD_color_multiply_scalar((*iter)->col, color_coef));
+                TCOD_console_print(engine->gui->con, startx, y, (*iter)->text);
+                y++;
+                if(color_coef < 1.0f) {
+                        color_coef += 0.3f;
+                }
+        }
 }
 
 static void gui_render(struct engine *engine){
@@ -21,18 +38,18 @@ static void gui_render(struct engine *engine){
                                 engine->player->destructible->hp,
                                 engine->player->destructible->max_hp,
                                 TCOD_light_red, TCOD_darker_red);
-
+        engine->gui->render_log(engine, MSG_X, 1);
         TCOD_console_blit(engine->gui->con, 0, 0, engine->window_w, PANEL_H,
                           NULL, 0, engine->window_h - PANEL_H, 1.f, 1.f);
-        printf("window_w = %d\nwindow_h = %d\npanel_h = %d\n", engine->window_w,
-               engine->window_h, PANEL_H);
 }
 
 void init_gui(struct gui **gui, int w, int h){
         struct gui *tmp = malloc(sizeof *tmp);
         tmp->con = TCOD_console_new(w, h);
         tmp->render_bar = render_bar;
+        tmp->render_log = render_log;
         tmp->render = gui_render;
+        tmp->log = TCOD_list_new();
         *gui = tmp;
 }
                       
--- src/gui.h
+++ src/gui.h
@@ -5,7 +5,14 @@
 
 enum{
         PANEL_H = 7,
-        BAR_W = 20
+        BAR_W = 20,
+        MSG_X = BAR_W + 2,
+        MSG_HEIGHT = PANEL_H - 1
+};
+
+struct message{
+        char *text;
+        TCOD_color_t col;
 };
 
 struct gui{
@@ -15,6 +22,8 @@ struct gui{
                            const char *name, const float value,
                            const float max_value, const TCOD_color_t bar_col,
                            const TCOD_color_t back_col);
+        void (*render_log)(struct engine *engine, int startx, int starty);
+        TCOD_list_t *log;
 };
 
 void init_gui(struct gui **gui, int w, int h);
