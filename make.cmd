53a2ee83668360955a69f452527c0d110622028d
Sergi Pasoev <s.pasoev@gmail.com>
2016-07-31 16:58:21 +0400

    add windows make command

---
+++ make.cmd
@@ -0,0 +1,8 @@
+cd C:\projects\gamedev\rogue\innfin\build
+"C:\Program Files (x86)\JetBrains\CLion 2016.1.3\bin\cmake\bin\cmake.exe" --build C:\Users\Sergi\.CLion2016.1\system\cmake\generated\innfin-868abd7a\868abd7a\Debug --target all -- -j 4
+
+cd C:\Users\Sergi\.CLion2016.1\system\cmake\generated\innfin-868abd7a\868abd7a\Debug
+C:\MinGW\bin\mingw32-make.exe
+cd C:\projects\gamedev\rogue\innfin\build
+Debug\innfin
+cd ..
