fa1441736233f8d43ea8407d82e846127e5fa68e
Sergo Pasoevi <spasoevi@evolutiongaming.com>
2019-01-02 23:13:14 +0200

    Download libs for mac

--- getlibs.sh
+++ getlibs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-curl -LO https://github.com/pasoev/innfin/releases/download/v0.0.4/libs_linux.tar.gz
-tar xf libs_linux.tar.gz
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=linux;;
+    Darwin*)    machine=mac;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+curl -LO https://github.com/pasoev/innfin/releases/download/v0.0.4/libs_${machine}.tar.gz
+tar xf libs_${machine}.tar.gz
 ln -s lib32 lib
